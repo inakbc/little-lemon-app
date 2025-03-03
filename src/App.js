1075448de85e5b820d3c02f779b6271c6a6594b1
Inna Kebchuk <innakbc@Innas-MacBook-Pro.local>
2025-03-02 19:20:03 -0800

    updating Header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import Header from "./components/Header";
+import Header from "./components/Header/Header";
 import Hero from "./components/Hero";
 import SpecialsCard from "./components/SpecialCard";
 // import { Routes, Route, Link } from "react-router-dom";
---
+++ src/components/Header/tabs-list.js
@@ -0,0 +1,26 @@
+export const DEFAULT_TABS = [
+    {
+        url: "/home",
+        title: "Home",
+    },
+    {
+        url: "/about",
+        title: "About",
+    },
+    {
+        url: "/menu",
+        title: "Menu",
+    },
+    {
+        url: "/reservations",
+        title: "Reservations",
+    },
+    {
+        url: "/order-online",
+        title: "Order Online",
+    },
+    {
+        url: "/login",
+        title: "Login",
+    },
+];
