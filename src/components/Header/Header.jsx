692312a5559e029e5b90de71d547431f55f04264
Inna Kebchuk <innakbc@Innas-MacBook-Pro.local>
2025-03-14 20:07:43 -0700

    changes to the header container

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,21 @@
 import "./Header.css";
 import Logo from "../../assets/icons/Logo.png";
+import Basket from "../../assets/icons/Basket.svg";
 import Nav from "./Nav";
 
 const Header = () => {
     return (
-        <header>
+        <header className="header">
             <div className="logo">
                 <img src={Logo} alt="Lemon Logo" />
             </div>
             <Nav />
+            <div className="header-actions">
+                <button className="order-button">Order Online</button>
+                <button className="cart-icon">
+                    <img src={Basket} alt="Basket" className="basket" />
+                </button>
+            </div>
         </header>
     );
 };
